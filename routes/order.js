1757de5f16c2a28abccfd4047d67ad1324058758
edgcu <edgcuong@gmail.com>
2022-11-24 16:57:04 +0700

    update request

--- routes/order.js
+++ routes/order.js
@@ -57,7 +57,7 @@ module.exports = function(astraClient) {
     router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
         try {
             const deletedOrder = await collection.delete(req.params.id);
-            return res.status(200).json("Order has been deleted...", deletedOrder);
+            return res.status(200).json(deletedOrder);
         } catch (err) {
             return res.status(500).json(err);
         }
