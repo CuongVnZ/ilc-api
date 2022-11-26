4d7faf102296e17b941f37b495e36b8e0c46fd66
edgcu <edgcuong@gmail.com>
2022-11-26 15:00:26 +0700

    update

--- routes/stripe.js
+++ routes/stripe.js
@@ -10,6 +10,7 @@ router.post("/payment", (req, res) => {
         currency: "usd"
     }, (stripeErr, stripeRes) => {
         if (stripeErr) {
+            console.log(stripeErr)
             return res.status(500).json(stripeErr);
         }
         return res.status(200).json(stripeRes);
